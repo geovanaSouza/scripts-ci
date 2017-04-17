1278e32d7d721233cee7a375702d05fc6312dc7c
Geovana Possenti de Souza <geovana.desouza@thomsonreuters.com>
2017-04-17 12:17:34 -0300

    Add steps to change config and scripts dir

--- maven.sh
+++ maven.sh
@@ -28,21 +28,20 @@ mvn_execute(){
   echo PROJECT_NAME=${PROJECT_NAME}
   echo MVN_ARGS=${MVN_ARGS}
 
-  cd $WORKSPACE/src/${PROJECTS_DIR}/${PROJECT_NAME}
+
   if [[ "$MVN_ARGS" == 'manual_replace' ]]; then
     xmlstarlet edit -L -N pom=http://maven.apache.org/POM/4.0.0 -u '/pom:project/pom:parent/pom:version' -v "${PROJECT_VERSION}" pom.xml
   else
     mvn ${MVN_ARGS}
   fi
-  cd $WORKSPACE
 }
 
 
-PROJECT_COUNT=$(jq ".$ACTION | length" $WORKSPACE/src/.maven-ci.json)
+PROJECT_COUNT=$(jq ".$ACTION | length" $WORKSPACE/.maven-ci.json)
 
 for (( key=0; key<${PROJECT_COUNT}; key++ ))
 do
-  PROJECT_NAME=$(jq ".$ACTION[$key].project" $WORKSPACE/src/.maven-ci.json | tr -d '"')
-  MVN_ARGS=$(jq ".$ACTION[$key].task" $WORKSPACE/src/.maven-ci.json | tr -d '"')
+  PROJECT_NAME=$(jq ".$ACTION[$key].project" $WORKSPACE/.maven-ci.json | tr -d '"')
+  MVN_ARGS=$(jq ".$ACTION[$key].task" $WORKSPACE/.maven-ci.json | tr -d '"')
   mvn_execute $PROJECT_NAME "${MVN_ARGS}"
 done
