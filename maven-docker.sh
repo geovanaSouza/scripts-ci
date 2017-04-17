59f5c86f3e020c4ce99c8e23aa60643fc61769e9
Geovana Possenti de Souza <geovana.desouza@thomsonreuters.com>
2017-04-17 12:14:40 -0300

    Add steps to change config and scripts dir

--- maven-docker.sh
+++ maven-docker.sh
@@ -18,6 +18,8 @@
 #  ./maven-docker.sh dist'
 
 ACTION=$1
+SCRITPS_CI_PATH=$JENKINS_HOME/workspace/scripts-ci
+CONFIGS_CI_PATH=$JENKINS_HOME/workspace/configs-ci
 
 #docker_stop(){
 #  CONTAINER_NAME=$1
@@ -39,19 +41,21 @@ mvn_execute(){
   echo PROJECT_NAME=${PROJECT_NAME}
   echo MVN_ARGS=${MVN_ARGS}
 
-  cd $WORKSPACE/src/
   #docker_stop ${CONTAINER_NAME}
-  
-  docker run --rm  -v "$PWD":/usr/src/mymaven -v "$WORKSPACE/scripts/.bowerrc":/root/.bowerrc -v "$WORKSPACE/scripts/$CONFIG_APPLICATION/maven-settings.xml":/usr/share/maven/ref/settings.xml -v "$JENKINS_HOME/.m2/repository":/usr/share/maven/repository -w /usr/src/mymaven maven:3-jdk-7 /bin/bash -c "cd ${PROJECTS_DIR}/${PROJECT_NAME}; mvn ${MVN_ARGS}"
-#--name ${CONTAINER_NAME}
+  if [[ "$MVN_ARGS" == 'manual_replace' ]]; then
+    xmlstarlet edit -L -N pom=http://maven.apache.org/POM/4.0.0 -u '/pom:project/pom:parent/pom:version' -v "${PROJECT_VERSION}" ${PROJECTS_DIR}/${PROJECT_NAME}/pom.xml
+  else
+    docker run --rm  -v "$PWD":/usr/src/mymaven -v "SCRITPS_CI_PATH/.bowerrc":/root/.bowerrc -v "$CONFIGS_CI_PATH/maven/$CONFIG_APPLICATION/maven-settings.xml":/usr/share/maven/ref/settings.xml -v "$JENKINS_HOME/.m2/repository":/usr/share/maven/repository -w /usr/src/mymaven maven:3-jdk-7 /bin/bash -c "cd ${PROJECTS_DIR}/${PROJECT_NAME}; mvn ${MVN_ARGS}"
+    #--name ${CONTAINER_NAME}
+  fi
 }
 
 
-PROJECT_COUNT=$(jq ".$ACTION | length" $WORKSPACE/src/.maven-ci.json)
+PROJECT_COUNT=$(jq ".$ACTION | length" $WORKSPACE/.maven-ci.json)
 
 for (( key=0; key<${PROJECT_COUNT}; key++ ))
 do  
-  PROJECT_NAME=$(jq ".$ACTION[$key].project" $WORKSPACE/src/.maven-ci.json | tr -d '"')
-  MVN_ARGS=$(jq ".$ACTION[$key].task" $WORKSPACE/src/.maven-ci.json | tr -d '"')
+  PROJECT_NAME=$(jq ".$ACTION[$key].project" $WORKSPACE/.maven-ci.json | tr -d '"')
+  MVN_ARGS=$(jq ".$ACTION[$key].task" $WORKSPACE/.maven-ci.json | tr -d '"')
   mvn_execute $PROJECT_NAME "${MVN_ARGS}"
 done
