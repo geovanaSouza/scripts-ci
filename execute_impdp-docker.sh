f4f21c46dec4ff6bb788291ece719aa41c35d0be
Geovana Possenti de Souza <geovana.desouza@thomsonreuters.com>
2017-04-20 11:30:18 -0300

    Add datapump parameters

--- execute_impdp-docker.sh
+++ execute_impdp-docker.sh
@@ -4,7 +4,7 @@ DB_USER=$1
 DB_PWD=$2
 DB_HOST=$3
 DB_SVC_NAME=$4
-DP_DIR=$5
-DP_FILE=$6
+DP_PARAMETERS=$5
 
-docker run --rm geovanasouza/oracle-client:12.1.0.2.1 impdp $DB_USER/$DB_PWD@$DB_HOST:1521/$DB_SVC_NAME DIRECTORY=${DP_DIR} DUMPFILE=${DP_FILE}
+
+docker run --rm geovanasouza/oracle-client:12.1.0.2.1 impdp $DB_USER/$DB_PWD@$DB_HOST:1521/$DB_SVC_NAME ${DP_PARAMETERS}
