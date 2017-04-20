be057da717d4c2e3df35aef4a8840000fadf501f
Geovana Possenti de Souza <geovana.desouza@thomsonreuters.com>
2017-04-20 09:03:47 -0300

    Change docker image to execute impdp

--- execute_impdp-docker.sh
+++ execute_impdp-docker.sh
@@ -7,4 +7,4 @@ DB_SVC_NAME=$4
 DP_DIR=$5
 DP_FILE=$6
 
-docker run --rm geovanasouza/jdk-orcl_oracleclient:7 impdp $DB_USER/$DB_PWD@$DB_HOST:1521/$DB_SVC_NAME DIRECTORY=${DP_DIR} DUMPFILE=${DP_FILE}
+docker run --rm geovanasouza/oracle-client:12.1.0.2.1 impdp $DB_USER/$DB_PWD@$DB_HOST:1521/$DB_SVC_NAME DIRECTORY=${DP_DIR} DUMPFILE=${DP_FILE}
